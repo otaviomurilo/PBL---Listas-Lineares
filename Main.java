a10b78e26c6396793f8c3149aa4392eccd29dd10
Otavio Murilo <132911950+otaivomuirlo@users.noreply.github.com>
2025-03-20 11:24:17 -0300

    main

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
-import java.util.LinkedList;
-
 public class Main {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
         Queue<Integer> queue = new Queue<>();
-
-        String inputFilePath = "assets/image.png";
-        String outputFilePath = "assets/result.png";
 
 
     }
