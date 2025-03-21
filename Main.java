bfe9331f0dc09ff975bf36ce0e5ddd2bda90a284
Otavio Murilo <132911950+otaivomuirlo@users.noreply.github.com>
2025-03-21 14:10:14 -0300

    Main

--- Main.java
+++ Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         ImageManager.clearFolder();
-
+        try {
+            FloodFill floodFill = new FloodFill("assets/input.png");
+            floodFill.fillWithStack(500, 650, 0xFF00FF00, 500, 700); 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
