c9ba5c667a688b67274f9d43e1cb3e48bd99334b
Theo <165860083+TheoRavaglia@users.noreply.github.com>
2025-03-22 18:17:58 -0300

    floodfill com fila

--- FloodFill.java
+++ FloodFill.java
@@ -1,66 +1,100 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Stack;
 import javax.imageio.ImageIO;
 
 public class FloodFill {
     private BufferedImage image;
     private int width;
     private int height;
-    private boolean useStacks;
 
-    public FloodFill(String imagePath) throws IOException{
+    public FloodFill(String imagePath) throws IOException {
         image = ImageIO.read(new File(imagePath));
         width = image.getWidth();
         height = image.getHeight();
     }
 
-    public void fillWithStack(int initX, int initY, int newColour, int transitionImg, int transitionInterval) throws IOException {
+    // ========== IMPLEMENTAÇÃO COM PILHA (DFS) ========== //
+    public void fillWithStack(int initX, int initY, int newColour, int transitionInterval) throws IOException {
         try {
             int initColour = image.getRGB(initX, initY);
-            
+
             Stack<int[]> stack = new Stack<>();
-            stack.push(new int[]{ initX, initY });
+            stack.stackPush(new int[]{ initX, initY });
             int pixelsModified = 0;
-        
+
             while (!stack.isEmpty()) {
-                int[] pixel = stack.pop();
+                int[] pixel = stack.stackPop();
                 int x = pixel[0];
                 int y = pixel[1];
-    
+
                 if (isValidPixel(x, y, initColour)) {
                     image.setRGB(x, y, newColour);
                     pixelsModified++;
-                    
-                    stack.push(new int[]{ x + 1, y });
-                    stack.push(new int[]{ x - 1, y });
-                    stack.push(new int[]{ x, y + 1 });
-                    stack.push(new int[]{ x, y - 1 });
-        
+
+                    // Adiciona vizinhos (ordem inversa para garantir DFS)
+                    stack.stackPush(new int[]{ x + 1, y });
+                    stack.stackPush(new int[]{ x - 1, y });
+                    stack.stackPush(new int[]{ x, y + 1 });
+                    stack.stackPush(new int[]{ x, y - 1 });
+
+                    // Salva transição
                     if (pixelsModified % transitionInterval == 0) {
-                        ImageManager.saveTransitionImage(image, "output_transition_" + pixelsModified + ".png");
+                        ImageManager.saveTransitionImage(image, "stack_transition_" + pixelsModified + ".png");
                     }
                 }
             }
-            ImageManager.saveFinalImage(image, "output.png");
-        }  catch (ArrayIndexOutOfBoundsException e) {
+            ImageManager.saveFinalImage(image, "output_stack.png");
+        } catch (ArrayIndexOutOfBoundsException e) {
             indexOutOfBounds();
         }
     }
-    
-    private boolean isValidPixel(int x, int y, int initColour) {
-        if (x < 0 || x >= image.getWidth() || y < 0 || y >= image.getHeight()) {
-            return false; 
+
+    // ========== IMPLEMENTAÇÃO COM FILA (BFS) ========== //
+    public void fillWithQueue(int initX, int initY, int newColour, int transitionInterval) throws IOException {
+        try {
+            int initColour = image.getRGB(initX, initY);
+
+            Queue<int[]> queue = new Queue<>();
+            queue.queuePush(new int[]{ initX, initY });
+            int pixelsModified = 0;
+
+            while (!queue.isEmpty()) {
+                int[] pixel = queue.queuePop();
+                int x = pixel[0];
+                int y = pixel[1];
+
+                if (isValidPixel(x, y, initColour)) {
+                    image.setRGB(x, y, newColour);
+                    pixelsModified++;
+
+                    // Adiciona vizinhos (ordem normal para garantir BFS)
+                    queue.queuePush(new int[]{ x + 1, y });
+                    queue.queuePush(new int[]{ x - 1, y });
+                    queue.queuePush(new int[]{ x, y + 1 });
+                    queue.queuePush(new int[]{ x, y - 1 });
+
+                    // Salva transição
+                    if (pixelsModified % transitionInterval == 0) {
+                        ImageManager.saveTransitionImage(image, "queue_transition_" + pixelsModified + ".png");
+                    }
+                }
+            }
+            ImageManager.saveFinalImage(image, "output_queue.png");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            indexOutOfBounds();
         }
-        
-        return image.getRGB(x, y) == initColour; 
+    }
+
+    // ========== MÉTODOS AUXILIARES ========== //
+    private boolean isValidPixel(int x, int y, int initColour) {
+        return x >= 0 && x < width &&
+                y >= 0 && y < height &&
+                image.getRGB(x, y) == initColour;
     }
 
     private void indexOutOfBounds() {
-        System.out.println("Erro: A coordenada fornecida não existe na imagem.");
-        System.out.println("Coordenadas válidas: 0 <= X < " + width + ", 0 <= Y < " + height);
+        System.out.println("Erro: Coordenadas fora dos limites da imagem!");
+        System.out.println("Dimensões válidas: 0 <= X < " + width + ", 0 <= Y < " + height);
     }
-
 }
-
--- Main.java
+++ Main.java
@@ -1,12 +1,17 @@
 import java.io.IOException;
 
 public class Main {
-
     public static void main(String[] args) {
         ImageManager.clearFolder();
         try {
-            FloodFill floodFill = new FloodFill("assets/input.png");
-            floodFill.fillWithStack(200, 150, 0xFF00FFF0, 500, 700); 
+            FloodFill floodFillStack = new FloodFill("assets/input.png");
+            // 4 argumentos: x, y, cor, intervalo de transição
+            floodFillStack.fillWithStack(200, 150, 0xFF00FF00, 500);
+
+            FloodFill floodFillQueue = new FloodFill("assets/input.png");
+            // 4 argumentos: x, y, cor, intervalo de transição
+            floodFillQueue.fillWithQueue(200, 150, 0xFFFF0000, 500);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
